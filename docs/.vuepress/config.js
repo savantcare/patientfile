5ee887c96d609e3b32186a2041bf8ca3649ec6c0
Vikas <kedia.vikas@gmail.com>
2020-05-30 22:42:20 -0700

    more readable

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,12 @@
 module.exports = {
+  title: 'Patient file documentation',
   plugins: ['vuepress-plugin-table-of-contents'],
   themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Savant', link: 'https://www.savantcare.com/' },
+      { text: 'Repo', link: 'https://www.github.com/savantcare/patientfile' }
+    ],
     sidebar: [
       '/',
       ['/tech/','Tech why'],
