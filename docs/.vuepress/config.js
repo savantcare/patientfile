500018c3b2809758c677f2978a8c2a24aa8cc2cb
Vikas <kedia.vikas@gmail.com>
2020-05-30 22:58:32 -0700

    more readable

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,12 +8,43 @@ module.exports = {
       { text: 'Repo', link: 'https://www.github.com/savantcare/patientfile' }
     ],
     sidebar: [
-      '/',
-      ['/tech/','Tech why'],
-      ['/ui/', 'UI'],
-      ['/components/','Components'],
-      ['/todo', 'Todo'],
-     
+      {
+        title: 'Home',   // required
+        path: '/',      // optional, link of the title, which should be an absolute path and must exist
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+      },
+      {
+        title: 'Tech',   // required
+        path: '/tech/',      // optional, link of the title, which should be an absolute path and must exist
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+        children: [
+          ['/tech/howto','How'],
+        ]
+      },
+      {
+        title: 'Components',
+        path: '/components/',      // optional, link of the title, which should be an absolute path and must exist
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+        children: [ 
+          ['/components/health/','Health'],
+          ['/components/others/','Others'],
+        ]
+      },
+      {
+        title: 'UI',
+        path: '/ui/',      // optional, link of the title, which should be an absolute path and must exist
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+      },
+      {
+        title: 'Todo',
+        path: 'todo',      // optional, link of the title, which should be an absolute path and must exist
+        collapsable: false, // optional, defaults to true
+        sidebarDepth: 1,    // optional, defaults to 1
+      }
     ]
   }
 }
