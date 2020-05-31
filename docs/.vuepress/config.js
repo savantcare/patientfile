ca511622daf7a7a9187c30f4d3f3040833aad59b
Vikas <kedia.vikas@gmail.com>
2020-05-30 17:56:21 -0700

    Now there is a sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,10 @@
 module.exports = {
   plugins: ['vuepress-plugin-table-of-contents'],
+  themeConfig: {
+    sidebar: [
+      '/',
+      ['/todo', 'Todo'],
+      ['/tech-why','Tech why']
+    ]
+  }
 }
