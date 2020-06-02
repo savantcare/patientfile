56504d1a36fc57e39beaf38067cb9a5b522d5d9b
Alexey <alexeydimov1221@gmail.com>
2020-06-02 08:34:57 +0800

    Add animation to the CardHeader actions

--- vue-client/src/components/common/CardHeader.vue
+++ vue-client/src/components/common/CardHeader.vue
@@ -7,57 +7,58 @@
     https://www.youtube.com/watch?v=1GYsKWyA7LA
     https://vuejs.org/v2/api/#transition
     -->
+    <transition name="fade">
+      <div style="float: right" v-if="showActions">
+        <el-button
+          type="text"
+          size="mini"
+          @click="$emit('multiDiscontinue')"
+          v-if="selected.length > 0"
+        >D</el-button>
+        <div v-else>
+          <el-button type="text" size="mini" @click="$emit('showAddDialog')">A</el-button>
+          <el-button type="text" size="mini" @click="$emit('showMultiChangeDialog')">M</el-button>
+          <el-button type="text" size="mini" @click="$emit('focusPanel')">F</el-button>
 
-    <div style="float: right" v-if="showActions">
-      <el-button
-        type="text"
-        size="mini"
-        @click="$emit('multiDiscontinue')"
-        v-if="selected.length > 0"
-      >D</el-button>
-      <div v-else>
-        <el-button type="text" size="mini" @click="$emit('showAddDialog')">A</el-button>
-        <el-button type="text" size="mini" @click="$emit('showMultiChangeDialog')">M</el-button>
-        <el-button type="text" size="mini" @click="$emit('focusPanel')">F</el-button>
-
-        <!--
+          <!--
         TODO: Clicking on settings icon will allow choosing which columns to display and 
         save preferences in local storage
         This will need to know what columns are available in the JSON. 
         Component 1 might have 5 columns whose visibility can be turned on or off
         and component 2 might have 2 columns whose visibility can be turned on or off
-        -->
-        <!--<el-popover placement="bottom" width="200" trigger="click">
+          -->
+          <!--<el-popover placement="bottom" width="200" trigger="click">
           <i slot="reference" class="el-icon-s-tools settingsIcon"></i>
-        </el-popover>-->
+          </el-popover>-->
 
-        <el-popover
-          placement="bottom"
-          width="200"
-          trigger="click"
-          @show="showPopover = true"
-          @hide="showPopOver = false"
-        >
-          <el-select
-            v-model="selectedColumn"
-            size="mini"
-            clearable
-            multiple
-            placeholder="Select"
-            collapse-tags
-            @change="handleChangeSelect"
+          <el-popover
+            placement="bottom"
+            width="200"
+            trigger="click"
+            @show="showPopover = true"
+            @hide="showPopOver = false"
           >
-            <el-option
-              v-for="item in columns"
-              :key="item.field"
-              :label="item.label"
-              :value="item.field"
-            ></el-option>
-          </el-select>
-          <i slot="reference" class="el-icon-s-tools settingsIcon"></i>
-        </el-popover>
+            <el-select
+              v-model="selectedColumn"
+              size="mini"
+              clearable
+              multiple
+              placeholder="Select"
+              collapse-tags
+              @change="handleChangeSelect"
+            >
+              <el-option
+                v-for="item in columns"
+                :key="item.field"
+                :label="item.label"
+                :value="item.field"
+              ></el-option>
+            </el-select>
+            <i slot="reference" class="el-icon-s-tools settingsIcon"></i>
+          </el-popover>
+        </div>
       </div>
-    </div>
+    </transition>
   </div>
 </template>
 
@@ -112,5 +113,12 @@ export default {
   color: #409eff;
   cursor: pointer;
   padding: 5px;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
 }
 </style>
