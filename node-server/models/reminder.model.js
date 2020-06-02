9247a79c825568b0650488711e1e84df29fbb297
ss <sanjaysaha@grmtech.com>
2020-06-02 20:30:31 +0530

    reminder right panel component

--- node-server/models/reminder.model.js
+++ node-server/models/reminder.model.js
@@ -1,73 +1,73 @@
-// module.exports = (sequelize, Sequelize) => {
-//   const Reminder = sequelize.define("reminder", {
-//     id: {
-//       type: Sequelize.INTEGER,
-//       primaryKey: true,
-//       autoIncrement: true
-//     },
-//     reminderID: {
-//       type: Sequelize.STRING
-//     },
-//     description: {
-//       type: Sequelize.STRING
-//     },
-//     discontinue: {
-//       type: Sequelize.BOOLEAN,
-//       defaultValue: 0
-//     },
-//     patientId: {
-//       type: Sequelize.STRING
-//     },
-//     createdByUserId: {
-//       type: Sequelize.STRING
-//     },
-//     discontinuedByUserId: {
-//       type: Sequelize.STRING
-//     },
-//     // Timestamps
-//     createdAt: Sequelize.DATE,
-//     updatedAt: Sequelize.DATE,
-//     discontinueAt: Sequelize.DATE
-//   });
-
-//   return Reminder;
-// };
-
 module.exports = (sequelize, Sequelize) => {
-const Reminder = sequelize.define("reminder", {
-uuid: {
-type: Sequelize.STRING,
-primaryKey: true
-},
-patientUUID: {
-type: Sequelize.STRING
-},
-description: {
-type: Sequelize.STRING
-},
-startDate: {
-type: Sequelize.DATE
-},
-discontinuedNotes: {
-type: Sequelize.STRING,
-defaultValue: null
-},
-recordChangedByUUID: {
-type: Sequelize.STRING
-},
-recordChangedOnDateTime: {
-type: Sequelize.DATE,
-defaultValue: Sequelize.NOW
-},
-recordChangedFromIPAddress: {
-type: Sequelize.STRING,
-defaultValue: null
-},
-recordChangedFromSection: {
-type: Sequelize.STRING,
-defaultValue: 'patientFile'
-}
-});
+  const Reminder = sequelize.define("reminder", {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    reminderID: {
+      type: Sequelize.STRING
+    },
+    description: {
+      type: Sequelize.STRING
+    },
+    discontinue: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: 0
+    },
+    patientId: {
+      type: Sequelize.STRING
+    },
+    createdByUserId: {
+      type: Sequelize.STRING
+    },
+    discontinuedByUserId: {
+      type: Sequelize.STRING
+    },
+    // Timestamps
+    createdAt: Sequelize.DATE,
+    updatedAt: Sequelize.DATE,
+    discontinueAt: Sequelize.DATE
+  });
 
-return Reminder;
+  return Reminder;
 };
+
+// module.exports = (sequelize, Sequelize) => {
+// const Reminder = sequelize.define("reminder", {
+// uuid: {
+// type: Sequelize.STRING,
+// primaryKey: true
+// },
+// patientUUID: {
+// type: Sequelize.STRING
+// },
+// description: {
+// type: Sequelize.STRING
+// },
+// startDate: {
+// type: Sequelize.DATE
+// },
+// discontinuedNotes: {
+// type: Sequelize.STRING,
+// defaultValue: null
+// },
+// recordChangedByUUID: {
+// type: Sequelize.STRING
+// },
+// recordChangedOnDateTime: {
+// type: Sequelize.DATE,
+// defaultValue: Sequelize.NOW
+// },
+// recordChangedFromIPAddress: {
+// type: Sequelize.STRING,
+// defaultValue: null
+// },
+// recordChangedFromSection: {
+// type: Sequelize.STRING,
+// defaultValue: 'patientFile'
+// }
+// });
+
+// return Reminder;
+// };
