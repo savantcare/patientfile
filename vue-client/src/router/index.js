c0c015ab723f2b2a8542c004743799bd66a47b2d
Vikas <kedia.vikas@gmail.com>
2020-06-07 15:31:41 -0700

    readable

--- vue-client/src/router/index.js
+++ vue-client/src/router/index.js
@@ -6,7 +6,7 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    name: 'PatientFile',
     component: () => import(/* webpackChunkName: "home" */'@/views/PatientFile.vue')
   },
   {
