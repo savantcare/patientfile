bdf258ae8ee42b6b78696ab2de330ab9c520e3f1
Vikas <kedia.vikas@gmail.com>
2020-06-10 02:18:13 -0700

    readable

--- node-server/copy-files-from-component-to-correct-locations.sh
+++ node-server/copy-files-from-component-to-correct-locations.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cp ../vue-client/src/components/patient/Recommendations/db/recommendation.database.js ./models/database/  
-cp ../vue-client/src/components/patient/Recommendations/db/recommendation.model.js ./models/
-cp ../vue-client/src/components/patient/Recommendations/db/recommendation.route.js ./routes/ 
+
+printf "== Copying 3 files from recommendation component "
+printf "." && cp ../vue-client/src/components/patient/Recommendations/db/recommendation.database.js ./models/database/  
+printf "." && cp ../vue-client/src/components/patient/Recommendations/db/recommendation.model.js ./models/
+printf "." && cp ../vue-client/src/components/patient/Recommendations/db/recommendation.route.js ./routes/ 
+echo " Done"
