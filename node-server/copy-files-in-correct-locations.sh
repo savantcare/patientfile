6039c011b7820c155155ef6cf43020bbb223b983
Vikas <kedia.vikas@gmail.com>
2020-06-10 01:58:47 -0700

    keepoing the db files with the component

---
+++ node-server/copy-files-in-correct-locations.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cp ../vue-client/src/components/patient/Recommendations/db/recommendation.database.js ./models/database/  
+cp ../vue-client/src/components/patient/Recommendations/db/recommendation.model.js ./models/
