9ad4b152e24de7cc3a61c3f9783fb3ad43061d75
anirban-grmtech <anirban@grmtech.com>
2020-06-18 17:48:42 +0530

    PROS components

--- vue-client/src/components/patient/PsychReviewOfSystems/components/ADHD.vue
+++ vue-client/src/components/patient/PsychReviewOfSystems/components/ADHD.vue
@@ -3,16 +3,16 @@
     <el-col>
       <el-card class="box-card">
         <div slot="header" class="clearfix">
-          <span>Appearence</span>
+          <span>ADHD</span>
           <el-button style="float: right; padding: 3px 0" type="text">All normal</el-button>
         </div>
-        <el-form :model="appearenceForm" ref="appearenceForm" class="demo-dynamic">
+        <el-form :model="adhdForm" ref="adhdForm" class="demo-dynamic">
           <el-form-item>
-            <el-checkbox-group v-model="checkboxAppearence">
+            <el-checkbox-group v-model="checkboxADHD">
               <!--  When opened in multi change format size="small" 
                 Ref: https://element.eleme.io/#/en-US/component/checkbox
               -->
-              <el-checkbox-button v-for="app in appearence" :label="app" :key="app">{{app}}</el-checkbox-button>
+              <el-checkbox-button v-for="app in adhd" :label="app" :key="app">{{app}}</el-checkbox-button>
             </el-checkbox-group>
             <!--  When opened in multi change min-rows=1 -->
             <el-input
@@ -26,7 +26,7 @@
             <!-- When opened in multi change format the Save button will not be there.
             Since the whole form will be controlled by one Save button
             -->
-            <el-button type="success" @click="submitForm('appearenceForm')" size="small">Save</el-button>
+            <el-button type="success" @click="submitForm('adhdForm')" size="small">Save</el-button>
           </el-form-item>
         </el-form>
       </el-card>
@@ -35,33 +35,43 @@
 </template>
 
 <script>
-const appearenceOptions = [
-  "Good grooming and hyegine",
-  "No apparent distree",
-  "Well developed, well nourished",
-  "Appears stated age",
-  "Appears younger than stated age",
-  "Appears older than stated age",
-  "Obese",
-  "Thin or cachetic",
-  "Disheveled, unkempt",
-  "Malodorous"
+const adhdOptions = [
+  "INATTENTION",
+  "HYPERACTIVITY",
+  "Careless mistakes or poor attention to detail",
+  "Problem sustaining attention",
+  "Poor listening",
+  "Fails to finish tasks",
+  "Difficulty organizing tasks",
+  "Reluctant to engage in tasks that require sustained mental effort",
+  "Loses things often",
+  "Distracted by extraneous stimuli",
+  "Forgetful daily activities",
+  "Fidgets often",
+  "Leaves seat often when sitting is expected",
+  "Feels restless",
+  "Unable to engage in leisurely activities quietly",
+  'Seems "on the go" or "driven by motor"',
+  "Talks excessively",
+  "Blurts out often",
+  "Difficulty waiting turn",
+  "Interrupts or intrudes on others"
 ];
 
 export default {
   data() {
     return {
-      appearenceForm: { recs: [{ description: "" }] },
+      adhdForm: { recs: [{ description: "" }] },
       // When form loads this will have the currently selected values from the DB
-      checkboxAppearence: [""],
-      appearence: appearenceOptions,
+      checkboxADHD: [""],
+      adhd: adhdOptions,
       textarea: ""
     };
   },
   methods: {
     onClickSave(rec) {
       // Actions are triggered with the store.dispatch method Ref: https://vuex.vuejs.org/guide/actions.html#dispatching-actions
-      this.$store.dispatch("dbUpdateAppearenceInSM", {
+      this.$store.dispatch("dbUpdateADHDInSM", {
         data: rec,
         notify: this.$notify
       });
--- vue-client/src/components/patient/PsychReviewOfSystems/components/GatewayMoodSymptoms.vue
+++ vue-client/src/components/patient/PsychReviewOfSystems/components/GatewayMoodSymptoms.vue
@@ -3,16 +3,20 @@
     <el-col>
       <el-card class="box-card">
         <div slot="header" class="clearfix">
-          <span>Appearence</span>
+          <span>Gateway Mood Symptoms</span>
           <el-button style="float: right; padding: 3px 0" type="text">All normal</el-button>
         </div>
-        <el-form :model="appearenceForm" ref="appearenceForm" class="demo-dynamic">
+        <el-form
+          :model="gatewayMoodSymptomsForm"
+          ref="gatewayMoodSymptomsForm"
+          class="demo-dynamic"
+        >
           <el-form-item>
-            <el-checkbox-group v-model="checkboxAppearence">
+            <el-checkbox-group v-model="checkboxGatewayMoodSymptoms">
               <!--  When opened in multi change format size="small" 
                 Ref: https://element.eleme.io/#/en-US/component/checkbox
               -->
-              <el-checkbox-button v-for="app in appearence" :label="app" :key="app">{{app}}</el-checkbox-button>
+              <el-checkbox-button v-for="app in gatewayMoodSymptoms" :label="app" :key="app">{{app}}</el-checkbox-button>
             </el-checkbox-group>
             <!--  When opened in multi change min-rows=1 -->
             <el-input
@@ -26,7 +30,11 @@
             <!-- When opened in multi change format the Save button will not be there.
             Since the whole form will be controlled by one Save button
             -->
-            <el-button type="success" @click="submitForm('appearenceForm')" size="small">Save</el-button>
+            <el-button
+              type="success"
+              @click="submitForm('gatewayMoodSymptomsForm')"
+              size="small"
+            >Save</el-button>
           </el-form-item>
         </el-form>
       </el-card>
@@ -35,33 +43,29 @@
 </template>
 
 <script>
-const appearenceOptions = [
-  "Good grooming and hyegine",
-  "No apparent distree",
-  "Well developed, well nourished",
-  "Appears stated age",
-  "Appears younger than stated age",
-  "Appears older than stated age",
-  "Obese",
-  "Thin or cachetic",
-  "Disheveled, unkempt",
-  "Malodorous"
+const gatewayMoodSymptomsOptions = [
+  "Q. In the past 10 days, how many days have you felt depressed for most of the day?",
+  "Q. In the past 10 days, how many days have you felt low interest or motivation throughout most of the day?",
+  "Q. In the past 10 days, how many days have you been abnormally irritable throughout most of the day?",
+  "Q. In the past 10 days, how many days have you been abnormally energetic or high energy throughout most of the day?",
+  "Q. In the past 10 days, how many days have you felt abnormal mood elevation throughout most of the day?",
+  "Q. How difficult to the above symptoms make your day to day life?"
 ];
 
 export default {
   data() {
     return {
-      appearenceForm: { recs: [{ description: "" }] },
+      gatewayMoodSymptomsForm: { recs: [{ description: "" }] },
       // When form loads this will have the currently selected values from the DB
-      checkboxAppearence: [""],
-      appearence: appearenceOptions,
+      checkboxGatewayMoodSymptoms: [""],
+      gatewayMoodSymptoms: gatewayMoodSymptomsOptions,
       textarea: ""
     };
   },
   methods: {
     onClickSave(rec) {
       // Actions are triggered with the store.dispatch method Ref: https://vuex.vuejs.org/guide/actions.html#dispatching-actions
-      this.$store.dispatch("dbUpdateAppearenceInSM", {
+      this.$store.dispatch("dbUpdateGatewayMoodSymptomsInSM", {
         data: rec,
         notify: this.$notify
       });
