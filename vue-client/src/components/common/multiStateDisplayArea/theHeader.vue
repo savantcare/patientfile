afa0df3ee5f9d28e407c95e6c52a43d917945787
Vikas <kedia.vikas@gmail.com>
2020-06-22 14:48:08 -0700

    improved doc

--- vue-client/src/components/common/multiStateDisplayArea/theHeader.vue
+++ vue-client/src/components/common/multiStateDisplayArea/theHeader.vue
@@ -79,7 +79,7 @@ Ref:  https://vuejs.org/v2/style-guide/#Single-instance-component-names-strongly
             When a component is mounted they create a array called
               componentNameEvalAtEachRowEnd
                 For e.g.  recommendationsEvalAtEachRowEnd
-                          weightsEvalAtEachRowEnd the structure is weightsEvalAtEachRowEnd[ROW_END_TIME] = data,time_of_eval
+                          weightsEvalAtEachRowEnd the structure is weightsEvalAtEachRowEnd[ROW_END_TIME] = data,time_of_eval,ROW_START_TIME
 
               From appt table:
 
@@ -110,7 +110,7 @@ Ref:  https://vuejs.org/v2/style-guide/#Single-instance-component-names-strongly
                 Key insight -> On graph doctors want to see data on eval time and not on entry time
 
                 To show data for an appointment note (timeOfStateToShow):
-                weightsEvalAtEachRowEnd filter where (index / ROW_END) > timeOfStateToShow (use nearest value) and ROW_START < timeOfStateToShow  
+                weightsEvalAtEachRowEnd filter where (index / ROW_END) >(if after) timeOfStateToShow (use nearest value) and ROW_START < (is before) timeOfStateToShow  
                     So the data set will be
                       For appt time 20th Feb 10AM    -> need to show data for 25th Feb 2 PM    -> data for ROW_END > 25th Feb 2 PM -> Hence data for ROW_END = 2038-01-19 03:14:07.999999 -> 185
                       For appt time 16th March 3 pm  -> need to show data for 21st March 1 PM -> data for ROW_END > 21st March 1 PM -> Hence data for ROW_END = 2038-01-19 03:14:07.999999 -> 185
