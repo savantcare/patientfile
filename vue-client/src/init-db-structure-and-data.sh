f6e8bf186f3151cbad316dc1923c4533c60d0867
Vikas <kedia.vikas@gmail.com>
2020-06-26 05:11:08 +0000

    bc

--- vue-client/src/init-db-structure-and-data.sh
+++ vue-client/src/init-db-structure-and-data.sh
@@ -7,7 +7,7 @@
 # Ref: https://stackoverflow.com/questions/51654041/loop-through-all-files-in-a-directory-and-subdirectories-using-bash
 for file in $(find . -type f -name "drop*.sql" -print)
 do
-    echo "== executing create DB $i";
+    echo "== executing drop DB $i";
     mysql -u $DB_USER --password=$DB_PASS < $file 
 done
 
@@ -21,13 +21,13 @@ done
 
 for file in $(find . -mindepth 3 -type f -name "structure*.sql" -print)
 do
-    echo "Executing the file $file"
+    echo "Executing create structure $file"
     mysql -u $DB_USER --password=$DB_PASS < $file 
 done
 
 
 for file in $(find . -mindepth 3 -type f -name "testdata*.sql" -print)
 do
-    echo "Executing the file $file"
+    echo "Executing create test data $file"
     mysql -u $DB_USER --password=$DB_PASS < $file 
 done
