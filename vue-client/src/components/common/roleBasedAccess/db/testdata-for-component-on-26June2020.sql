3ee1c53a735a9b4108f4549507b78752b3ec8c71
Vikas <kedia.vikas@gmail.com>
2020-06-26 09:50:58 -0700

    better name

--- vue-client/src/components/common/roleBasedAccess/db/testdata-for-component-on-26June2020.sql
+++ vue-client/src/components/common/roleBasedAccess/db/testdata-for-component-on-26June2020.sql
@@ -1,12 +1,9 @@
 
 
-INSERT INTO `ctMaster` (`
-uuid`,
-`tag
-`, `name`, `recordChangedByUUID`, `recordChangedFromIPAddress`) VALUES
+INSERT INTO `ctMaster` (`uuid`,`tag`, `name`, `recordChangedByUUID`, `recordChangedFromIPAddress`) VALUES
 (1, "health", 'recommendation', '1', '202.005.122.057'),
-(2, "other", 'reminder', '2', '202.005.122.057'),
-(3, "health", 'other', '3', '202.005.122.057');
+(2, "other", 'contacts', '2', '202.005.122.057'),
+(3, "health", 'reminder', '3', '202.005.122.057');
 
 INSERT INTO `ctAllowedToAccessByEachUserRole` (`
 uuid`,
@@ -15,3 +12,9 @@ uuid`,
 ('1', '897d25c6-2c84-47fe-9236-2c3cc9c70bdf', '1', '1', '3', '1', '10.10.10.1'),
 ('2', '897d25c6-2c84-47fe-9236-2c3cc9c70bdf', '2', '2', '2', '1', '10.10.10.1'),
 ('3', '897d25c6-2c84-47fe-9236-2c3cc9c70bdf', '3', '3', '1', '1', '10.10.10.1');
+
+
+INSERT INTO `search` (`uuid`, `componentUUID`, `searchText`, `action`, `recordChangedByUUID`, `recordChangedFromIPAddress`) VALUES
+('1', '1', 'recommendation', '', '1', '202.005.122.057'),
+('2', '2', 'contacts', '', '2', '202.005.122.057'),
+('3', '3', 'reminder', '', '3', '202.005.122.057');
