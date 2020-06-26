82d845cc35a102e06d108e42733e482c3fc48153
Vikas <kedia.vikas@gmail.com>
2020-06-25 23:06:48 -0700

    better security

--- vue-client/src/init-db-structure-and-data.sh
+++ vue-client/src/init-db-structure-and-data.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # How tom export the env variables
+# export DB_HOST="nmch.com"
+# export DB_PORT="3306"
+# export DB_USER="ja"
 # export DB_PASS="ja"
 
 
@@ -10,26 +13,26 @@ echo "Executing drop DB";
 for file in $(find . -type f -name "drop*.sql" -print)
 do
     echo "-- $file";
-    mysql -u $DB_USER --password=$DB_PASS < $file 
+    mysql --host=DB_HOST --port=DB_PORT -u $DB_USER --password=$DB_PASS < $file 
 done
 
 echo "Executing create DB $i";
 for file in $(find . -type f -name "create*.sql" -print)
 do
     echo "-- $file";
-    mysql -u $DB_USER --password=$DB_PASS < $file 
+    mysql --host=DB_HOST --port=DB_PORT -u $DB_USER --password=$DB_PASS < $file 
 done
 
 echo "Executing create structure $file"
 for file in $(find . -mindepth 3 -type f -name "structure*.sql" -print)
 do
     echo "-- $file"
-    mysql -u $DB_USER --password=$DB_PASS < $file 
+    mysql --host=DB_HOST --port=DB_PORT -u $DB_USER --password=$DB_PASS < $file 
 done
 
 echo "Executing create test data"
 for file in $(find . -mindepth 3 -type f -name "testdata*.sql" -print)
 do
     echo "-- $file"
-    mysql -u $DB_USER --password=$DB_PASS < $file 
+    mysql --host=DB_HOST --port=DB_PORT -u $DB_USER --password=$DB_PASS < $file 
 done
