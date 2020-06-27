cb3191f3ec7b7265740d9b33f6ea292bfc5fb8f4
Vikas <kedia.vikas@gmail.com>
2020-06-26 23:21:57 -0700

    removed unused fields from userRole model

--- vue-client/src/components/common/userRole/vuex-orm-model/userRole.js
+++ vue-client/src/components/common/userRole/vuex-orm-model/userRole.js
@@ -1,19 +1,15 @@
 import { Model } from "@vuex-orm/core";
 
 export default class UserRole extends Model {
-    // This is the name used as module name of the Vuex Store.
-    static entity = "userRole";
+  // This is the name used as module name of the Vuex Store.
+  static entity = "userRole";
 
-    // List of all fields (schema) of the post model. `this.attr` is used
-    // for the generic field type. The argument is the default value.
-    static fields() {
-        return {
-            uuid: this.attr(null),
-            name: this.attr(null),
-            componentsAllowedToAccess: this.attr(null),
-            multiStateDisplayAreaComponentLoadSequence: this.attr(null),
-            currentStateDisplayAreaComponentLoadSequence: this.attr(null),
-
-        };
-    }
+  // List of all fields (schema) of the post model. `this.attr` is used
+  // for the generic field type. The argument is the default value.
+  static fields() {
+    return {
+      uuid: this.attr(null),
+      name: this.attr(null),
+    };
+  }
 }
